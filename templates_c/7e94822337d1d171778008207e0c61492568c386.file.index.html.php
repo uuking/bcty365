d8e6ec9cda5719669b2e316228c914031bde3a89
uuking <1210889580@qq.com>
2015-03-17 23:08:39 +0800

    0317

---
+++ templates_c/7e94822337d1d171778008207e0c61492568c386.file.index.html.php
@@ -0,0 +1,156 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-03-17 19:14:26
+         compiled from "templates\index.html" */ ?>
+<?php /*%%SmartyHeaderCode:331755080c92a735c4-00957030%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '7e94822337d1d171778008207e0c61492568c386' => 
+    array (
+      0 => 'templates\\index.html',
+      1 => 1426590777,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '331755080c92a735c4-00957030',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_55080c92bf6195_69654156',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_55080c92bf6195_69654156')) {function content_55080c92bf6195_69654156($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ("header.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+
+<div class="row clearfix">
+	<div class="col-sm-4 col-md-4 column">
+		<div class="list-group">
+			 <a href="#" class="list-group-item active">首页</a>
+			<div class="list-group-item">
+				列表头部
+			</div>
+			<div class="list-group-item">
+				<h4 class="list-group-item-heading">
+					列表内容
+				</h4>
+				<p class="list-group-item-text">
+					...
+				</p>
+			</div>
+			<div class="list-group-item">
+				<span class="badge">14</span>产品展示
+			</div> <a class="list-group-item active"><span class="badge">14</span>案例展示</a>
+		</div>
+		<div class="list-group">
+			 <a href="#" class="list-group-item active">首页</a>
+			<div class="list-group-item">
+				列表头部
+			</div>
+			<div class="list-group-item">
+				<h4 class="list-group-item-heading">
+					列表内容
+				</h4>
+				<p class="list-group-item-text">
+					...
+				</p>
+			</div>
+			<div class="list-group-item">
+				<span class="badge">14</span>产品展示
+			</div> <a class="list-group-item active"><span class="badge">14</span>案例展示</a>
+		</div>
+	</div>
+	<div class="col-sm-8 col-md-8 column">
+		<div class="media">
+			 <a href="#" class="pull-left"><img src="skin/img/64.jpg" class="media-object" alt='' /></a>
+			<div class="media-body">
+				<h4 class="media-heading">
+					新标题
+				</h4> 这里是简介内容区域！
+				<div class="media">
+					 <a href="#" class="pull-left"><img src="skin/img/64.jpg" class="media-object" alt='' /></a>
+					<div class="media-body">
+						<h4 class="media-heading">
+							新标题
+						</h4> 这里是简介内容区域！
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="media">
+			 <a href="#" class="pull-left"><img src="skin/img/64.jpg" class="media-object" alt='' /></a>
+			<div class="media-body">
+				<h4 class="media-heading">
+					新标题
+				</h4> 这里是简介内容区域！
+				<div class="media">
+					 <a href="#" class="pull-left"><img src="skin/img/64.jpg" class="media-object" alt='' /></a>
+					<div class="media-body">
+						<h4 class="media-heading">
+							新标题
+						</h4> 这里是简介内容区域！
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row clearfix">
+	<div class="col-md-4 column">
+		<div class="list-group">
+			 <a href="#" class="list-group-item active">首页</a>
+			<div class="list-group-item">
+				列表头部
+			</div>
+			<div class="list-group-item">
+				<h4 class="list-group-item-heading">
+					列表内容
+				</h4>
+				<p class="list-group-item-text">
+					...
+				</p>
+			</div>
+			<div class="list-group-item">
+				<span class="badge">14</span>产品展示
+			</div> <a class="list-group-item active"><span class="badge">14</span>案例展示</a>
+		</div>
+	</div>
+	<div class="col-md-4 column">
+		<div class="list-group">
+			 <a href="#" class="list-group-item active">首页</a>
+			<div class="list-group-item">
+				列表头部
+			</div>
+			<div class="list-group-item">
+				<h4 class="list-group-item-heading">
+					列表内容
+				</h4>
+				<p class="list-group-item-text">
+					...
+				</p>
+			</div>
+			<div class="list-group-item">
+				<span class="badge">14</span>产品展示
+			</div> <a class="list-group-item active"><span class="badge">14</span>案例展示</a>
+		</div>
+	</div>
+	<div class="col-md-4 column">
+		<div class="list-group">
+			 <a href="#" class="list-group-item active">首页</a>
+			<div class="list-group-item">
+				列表头部
+			</div>
+			<div class="list-group-item">
+				<h4 class="list-group-item-heading">
+					列表内容
+				</h4>
+				<p class="list-group-item-text">
+					...
+				</p>
+			</div>
+			<div class="list-group-item">
+				<span class="badge">14</span>产品展示
+			</div> <a class="list-group-item active"><span class="badge">14</span>案例展示</a>
+		</div>
+	</div>
+</div>
+<?php echo $_smarty_tpl->getSubTemplate ('footer.html', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+<?php }} ?>
