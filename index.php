06f62e7bcaf3e45b33d726c68b84d0c872a4e7a6
uuking <1210889580@qq.com>
2015-03-24 22:52:46 +0800

    0324

--- index.php
+++ index.php
@@ -12,6 +12,12 @@ while ($infouwz=mysqli_fetch_assoc($sqluwz)) {
 $sqlbj=mysqli_query($connect,"select * from tb_bccdjj where mark=1");
 $infobj=mysqli_fetch_assoc($sqlbj);
 
+$sqlnewbccd=mysqli_query($connect,"select imageaddress, tb_bccd.id as bccdid,bccdname,owner,price,addtime,bbname from tb_bccd left join tb_bb on tb_bccd.bbid=tb_bb.id order by addtime desc limit 0,2");
+while ($infonewbccd=mysqli_fetch_assoc($sqlnewbccd)) {
+	$infonewbccds[]=$infonewbccd;
+}
+// p($infonewbccds);die;
+$smarty->assign('infonewbccds',$infonewbccds);
 $smarty->assign('infouwzs',$infouwzs);
 $smarty->assign('infobj',$infobj);
 $smarty->assign('infos',$infos);
